5fa0dbd3f650f0718f468e756fbbebac3e1c3de6
woraphonchan-hub <woraphon.chan@bumail.net>
2026-03-28 19:51:30 +0700

    fixbug

---
+++ Assets/Scrip/playermove.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float turnSpeed = 120f;
+    public float jumpForce = 5f;
+
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+
+        float h = Input.GetAxis("Horizontal");
+        transform.Rotate(0, h * turnSpeed * Time.deltaTime, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+        Vector3 forwardMove = transform.forward * moveSpeed;
+        rb.MovePosition(rb.position + forwardMove * Time.fixedDeltaTime);
+    }
+
+
+    void OnCollisionStay(Collision collision)
+    {
+        isGrounded = true;
+    }
+}
